0e5d321eb31ad94d97b828374fc4032fa6f5c372
Shivansh Joshi <121875820+shivanshjoshi08@users.noreply.github.com>
2024-06-27 10:07:38 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3+3) we are using node.js not browser
+
+console.log(3 + 3);
+
+console.log("Shivansh");
+
+let age = 20
+let isLoggedIn = false
+let State;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
---
+++ Basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail = "shivansh@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+// accountId = 2 //Not allowed
+
+console.log(accountId);
+
+// prefer not to use Var 
+
+accountEmail = "sj@sj.com"
+accountPassword = "11223344"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
