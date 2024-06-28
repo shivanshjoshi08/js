b1e2906fa2236631aeffbb2803ba228ff4146b44
Shivansh Joshi <121875820+shivanshjoshi08@users.noreply.github.com>
2024-06-28 13:16:34 +0000

    datatype summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// #Primitive
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIN = true
+const outesideTemp = null
+let UserEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 12345678901234n   // BigINt
+
+const heroes = ["shaktiman", "nagraj"];
+let myObj = {
+    name: "shivansh",
+    age: 20,
+}
+
+const myFunction = function () {
+
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outesideTemp);
+
+
+
+
+
+// #NonPrimitive or Reference
+// Arrays, Objects, Function
