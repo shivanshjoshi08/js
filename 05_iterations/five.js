126b6449f879cb0be8a05f5ee8da77a176bb66bf
Shivansh Joshi <121875820+shivanshjoshi08@users.noreply.github.com>
2024-07-02 10:13:18 +0000

    loops on arrays

---
+++ 05_iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["java", "cpp", "python", "ruby", "js"]
+
+// coding.forEach(function (item) { 
+//     console.log(item);
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+let myArray = ['flash', 'superman', 'batman'];
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr++;
+}
