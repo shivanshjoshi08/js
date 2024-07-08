eadffe646d2646e9f67160fa417fd88db9aa5f39
Shivansh Joshi <121875820+shivanshjoshi08@users.noreply.github.com>
2024-07-08 13:51:00 +0000

    mathpi

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -1,1 +1,16 @@
-Object 
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI')
+
+// console.log(descriptor);
+
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true,
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    writable: false,
+    enumerable: false,
+})
